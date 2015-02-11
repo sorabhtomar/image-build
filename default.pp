7cdfa6ec28687ef8e1977c23b73bc3b61149cd62
Ezekiel Smithburg <ezekiel@udacity.com>
2015-02-10 21:10:31 -0800

    switch to aws based builds for bootstrapping

--- default.pp
+++ default.pp
@@ -36,39 +36,12 @@ class leiningen {
 class image_build {
   class {'packer': }
 
-  Exec {
-    path => '/usr/bin',
-    cwd  => '/build',
-  }
-  exec {
-    'fetch vagrant':
-      command => 'wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb';
-    'fetch virtualbox':
-      command => 'wget http://download.virtualbox.org/virtualbox/4.3.20/virtualbox-4.3_4.3.20-96996~Ubuntu~raring_amd64.deb',
-      before  => Exec['do final apt-get install'];
-    'do final apt-get install':
-      command => 'apt-get install -f';
-  }
-
   package {
     'librarian':
       provider => 'gem',
       ensure   => present,
       require  => Package['ruby-dev'];
-    ['ruby-dev', 'virtualbox']:;
-    'virtualbox':
-      provider => 'dpkg',
-      source   => '/build/virtualbox-4.3_4.3.20-96996~Ubuntu~raring_amd64.deb',
-      require  => Exec['fetch virtualbox'];
-    'vagrant':
-      provider => 'dpkg',
-      source   => '/build/vagrant_1.7.2_x86_64.deb',
-      require  => Exec['fetch vagrant'];
-    # vagrant needed for getting our base image in place, at least for now. this should be
-    # abstracted out into a separate system later, probably.
-    # the base image can be fetched with this command:
-    #
-    # vagrant box add udacity/puppet-masterless https://atlas.hashicorp.com/udacity/boxes/puppet-masterless/versions/0.0.1/providers/virtualbox.box
+    'ruby-dev':;
   }
 }
 
