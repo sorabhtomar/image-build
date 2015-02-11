e298576d5dfceb8ee1332397bb8afa14ef716a9d
Ezekiel Smithburg <ezekiel@udacity.com>
2015-02-10 16:33:11 -0800

    just to be safe

---
+++ default.pp
@@ -0,0 +1,25 @@
+class build_dir {
+  file {'/build':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+  }
+}
+
+class leiningen {
+  include build_dir
+
+  vcsrepo {'/build/leiningen':
+    source => 'git@github.com:technomancy/leiningen.git',
+    provider => 'git',
+    revision => '2.5.1',
+  }
+
+  file {'/usr/local/bin/lein':
+    source => '/build/leiningen/bin/lein',
+    owner  => 'root',
+    group  => 'root',
+    mode   => 'ugo+rx, u+w',
+    require => Vcsrepo['/build/leiningen'],
+  }
+}
