cefa67acb9d9d7f4fb76dad906fbc31b6b0bf18d
Ezekiel Smithburg <ezekiel@udacity.com>
2015-02-10 20:20:45 -0800

    install newer vagrant

--- default.pp
+++ default.pp
@@ -36,12 +36,21 @@ class leiningen {
 class image_build {
   class {'packer': }
 
+  exec {'fetch vagrant':
+    command => '/usr/bin/wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb',
+    cwd     => '/build',
+  }
+
   package {
     'librarian':
       provider => 'gem',
       ensure   => present,
       require  => Package['ruby-dev'];
-    ['ruby-dev', 'virtualbox', 'vagrant']:;
+    ['ruby-dev', 'virtualbox']:;
+    'vagrant':
+      provider => 'dpkg',
+      source   => '/build/vagrant_1.7.2_x86_64.deb',
+      require  => Exec['fetch vagrant'];
     # vagrant needed for getting our base image in place, at least for now. this should be
     # abstracted out into a separate system later, probably.
     # the base image can be fetched with this command:
